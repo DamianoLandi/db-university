3ca8fee9ecbe6b69bd48a692a38d2fb2e0bb690b
DamianoLandi <damianolandi01@gmail.com>
2021-09-28 16:47:07 +0200

    Query GroupBy 3

--- queries.sql
+++ queries.sql
@@ -56,4 +56,8 @@ SELECT COUNT(`id`)
 FROM `teachers`
 GROUP BY `office_address`;
 
+-- Calcolare la media dei voti di ogni appello d'esame
 
+SELECT AVG(`vote`)
+FROM `exam_student`
+GROUP BY `exam_id`;
