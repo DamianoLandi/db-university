a7944a5678e6fb5114be51d941aebebd812fe40b
DamianoLandi <damianolandi01@gmail.com>
2021-09-28 15:46:44 +0200

    Query 2

--- queries.sql
+++ queries.sql
@@ -4,3 +4,8 @@ SELECT `name`, `surname`, `date_of_birth`
 FROM `students`
 WHERE year(`date_of_birth`) = 1990;
 
+-- Selezionare tutti i corsi che valgono più di 10 crediti 
+
+SELECT *
+FROM `courses`
+WHERE `cfu` > 10;
