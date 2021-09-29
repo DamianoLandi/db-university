70f6c55cfbf5a2da20ba7341239ff9fe40419732
DamianoLandi <damianolandi01@gmail.com>
2021-09-29 16:24:26 +0200

    Query Join 4

--- queries_join.sql
+++ queries_join.sql
@@ -19,3 +19,11 @@ FROM `course_teacher`
 JOIN `teachers` ON `teacher_id` = `teachers`.`id`
 JOIN `courses` ON `course_id` = `courses`.`id`
 WHERE `teachers`.`name` = 'Fulvio' AND `teachers`.`surname` = 'Amato';
+
+-- Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
+
+SELECT *
+FROM `students`
+JOIN `degrees` ON `degree_id` = `degrees`.`id`
+JOIN `departments` ON `degrees`.`department_id` = `departments`.`id`
+ORDER BY `students`.`surname` ASC, `students`.`name` ASC;
