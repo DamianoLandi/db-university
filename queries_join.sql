89f203bbf9c28af6ca98ea9723b1982158849552
DamianoLandi <damianolandi01@gmail.com>
2021-09-29 15:58:20 +0200

    Query Join 2

--- queries_join.sql
+++ queries_join.sql
@@ -5,3 +5,10 @@ FROM `students`
 JOIN `degrees` ON `degree_id` = `degrees`.`id`
 WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
 
+-- Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+
+SELECT *
+FROM `degrees`
+JOIN `departments` ON `department_id` = `departments`.`id`
+WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
+
